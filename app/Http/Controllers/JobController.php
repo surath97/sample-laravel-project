e290cbd7f55557cfc4b391d9a7541fc7851f6486
Surath Chathuranga <surathchathuranga97@gmail.com>
2025-06-16 23:23:23 +0530

    Day 23 - Inline authorization

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Job;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class JobController extends Controller
 {
@@ -63,6 +64,15 @@ class JobController extends Controller
     public function edit(Job $job)
     {
         // $selected_job = Job::find($id); --------------> Route Model Binding
+
+        if (Auth::guest()) {
+            return redirect('/login');
+        }
+
+        if($job->employer->user->isNot(Auth::user())){
+
+            abort(403);
+        }
 
         return view('jobs.edit', ['job' => $job]);
     }
