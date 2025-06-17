406400afd61044d26fa5f98f01c14cab1f60c443
Surath Chathuranga <surathchathuranga97@gmail.com>
2025-06-18 00:06:08 +0530

    Day 23 - Middleware authorizatin

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,23 @@ Route::view('/contact', 'contact');
 //     Route::delete('jobs/{job}', 'destroy');
 // });
 
-Route::resource('jobs', JobController::class);
+// Route::resource('jobs', JobController::class)->middleware('auth');
+
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('jobs/create', [JobController::class, 'create'])->middleware('auth');;
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::post('jobs', [JobController::class, 'store'])->middleware('auth');
+// Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])->middleware(['auth', 'can:edit-job,job']);
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])->middleware('auth')->can('edit-job', 'job');
+Route::patch('/jobs/{job}', [JobController::class, 'update']);
+Route::delete('jobs/{job}', [JobController::class, 'destroy']);
+
 
 // Auth
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
-Route::get('/login', [SessionController::class, 'create']);
+Route::get('/login', [SessionController::class, 'create'])->name('login');
 Route::post('/login', [SessionController::class, 'store']);
 Route::post('/logout', [SessionController::class, 'destroy']);
 
